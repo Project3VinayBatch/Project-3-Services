5ff8190eb70862c7bea9f6a3e20813f440c2831c
alejandroOro99 <alejandro.orozco@revature.net>
2021-06-02 11:04:42 -0400

    first

---
+++ src/main/java/com/example/app/project3server/Project3ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.project3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/project3server/Project3ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.project3server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
