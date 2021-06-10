01a8d5a4c6aabe25f368144ec1aa7cb83ebaaf3d
Phuc Huynh <43251722+Phuc15@users.noreply.github.com>
2021-06-10 11:13:58 -0700

    file uploading cleanup

--- src/main/java/com/revature/initiative/service/UploadServiceImpl.java
+++ src/main/java/com/revature/initiative/service/UploadServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileOutputStream;
