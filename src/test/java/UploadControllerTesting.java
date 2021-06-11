47f2eb309aeb53d530adf3576c1b06044ecbf656
Beekenstein <bkelly0905@gmail.com>
2021-06-11 15:55:02 -0400

    Testing

---
+++ src/test/java/UploadControllerTesting.java
@@ -0,0 +1,26 @@
+import com.revature.initiative.controller.UploadController;
+import com.revature.initiative.service.UploadService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UploadControllerTesting {
+
+    @Test
+    public void TestUploadController(){
+
+        //Creating a mock upload services and naming it amazonS3BucketService
+        UploadService amazonS3BucketService = mock(UploadService.class);
+
+        //Creating an Upload Controll and calling it Test
+        UploadController testSubject = new UploadController(amazonS3BucketService);
+        when(amazonS3BucketService.uploadFile(null, null,null )).thenReturn("this fails");
+
+        ResponseEntity<String> labRat = testSubject.uploadFile(null, null, null);
+        Assertions.assertNotNull(labRat);
+    }
+
+}
