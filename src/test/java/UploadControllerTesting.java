de467cfe3d26c78e8048311a884e6f672f7ae5e3
alejandroOro99 <82408845+alejandroOro99@users.noreply.github.com>
2021-06-15 11:11:08 -0400

    Update UploadControllerTesting.java

--- src/test/java/UploadControllerTesting.java
+++ src/test/java/UploadControllerTesting.java
@@ -9,31 +9,31 @@ import static org.mockito.Mockito.when;
 
 public class UploadControllerTesting {
 
-    @Test
-    public void TestUploadController(){
+//     @Test
+//     public void TestUploadController(){
 
-        //Creating a mock upload services and naming it amazonS3BucketService
-        UploadService amazonS3BucketService = mock(UploadService.class);
+//         //Creating a mock upload services and naming it amazonS3BucketService
+//         UploadService amazonS3BucketService = mock(UploadService.class);
 
-        //Commenting to use later to check out uploading to the DB
-        //UploadService amazonS3BucketService = new UploadServiceImpl(new User)
+//         //Commenting to use later to check out uploading to the DB
+//         //UploadService amazonS3BucketService = new UploadServiceImpl(new User)
 
-        //Creating an Upload Control and calling it Test
-        UploadController testSubject = new UploadController(amazonS3BucketService);
+//         //Creating an Upload Control and calling it Test
+//         UploadController testSubject = new UploadController(amazonS3BucketService);
 
 
-//        MultipartFile multipartFile = new MockMultipartFile("testfile.tmp", "Weddingidea".getBytes());
-//        File file = new File("D:\\Brian_Folder\\Project3_BackEnd_Bkelly\\Project-3-Services\\src\\test\\testfiles\\Wedding idea.PNG");
-//        try (OutputStream os = new FileOutputStream(file)){
-//            os.write(multipartFile.getBytes());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+// //        MultipartFile multipartFile = new MockMultipartFile("testfile.tmp", "Weddingidea".getBytes());
+// //        File file = new File("D:\\Brian_Folder\\Project3_BackEnd_Bkelly\\Project-3-Services\\src\\test\\testfiles\\Wedding idea.PNG");
+// //        try (OutputStream os = new FileOutputStream(file)){
+// //            os.write(multipartFile.getBytes());
+// //        } catch (Exception e) {
+// //            e.printStackTrace();
+// //        }
 
-        String url = "https://www.returningURL.com";
-        when(amazonS3BucketService.uploadFile( null, null,null )).thenReturn(url);
-        ResponseEntity<String> labRat = testSubject.uploadFile(null , null, null);
-        Assertions.assertEquals(labRat, ResponseEntity.ok(url));
-    }
+//         String url = "https://www.returningURL.com";
+//         when(amazonS3BucketService.uploadFile( null, null,null )).thenReturn(url);
+//         ResponseEntity<String> labRat = testSubject.uploadFile(null , null, null);
+//         Assertions.assertEquals(labRat, ResponseEntity.ok(url));
+//     }
 
 }
